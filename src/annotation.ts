1be92b0f28bae04acfb6b88a719d24d8a2a57992
yeaung276 <yeyintaung.ya276@gmail.com>
2023-02-03 14:33:19 +0700

    again

--- src/annotation.ts
+++ src/annotation.ts
@@ -1,10 +1,14 @@
 import {context, getOctokit} from '@actions/github';
 import * as core from '@actions/core';
 import { Message, Result } from 'sarif';
+import { splitEvery } from 'ramda';
+import { sendRequest } from './github';
 
-export type AnnotationLevel = 'notice' | 'warning' | 'failure';
+const MAX_ANNOTATIONS_PER_REQUEST = 40;
 
-type Annotation = {
+type AnnotationLevel = 'notice' | 'warning' | 'failure';
+
+export type Annotation = {
     path: string
     start_line: number
     end_line: number,
@@ -21,54 +25,15 @@ export async function publishAnnotation(toolName: string, annotations: Annotatio
     return
   }
   core.info(`publishing output for ${toolName}`)
-  const token = core.getInput('token')
-  const octokit = getOctokit(token)
-  let sha = context.sha
-  if (context.payload.pull_request) {
-    sha = context.payload.pull_request.head.sha
-  }
-  const request = {
-    ...context.repo,
-    ref: sha
+  for(const group of splitEvery(MAX_ANNOTATIONS_PER_REQUEST, annotations)){
+    sendRequest({
+      title: toolName,
+      summary: `${annotations.length} issue(s) found.`,
+      text: `${annotations.length} issue(s) found.`,
+      annotations: group,
+    })
   }
 
-  const result = await octokit.rest.checks.listForRef(request)
-  const exists = result.data.check_runs.find(check => check.name === context.workflow)
-  const accept = 'application/vnd.github.v3+json'
-  if (exists) {
-    core.info('check exists, reusing...')
-    const id = exists.id
-    await octokit.rest.checks.update({
-      ...context.repo,
-      accept,
-      conclusion: 'success',
-      check_run_id: id,
-      status: 'completed',
-      output: {
-          title: toolName,
-          summary: `${annotations.length} issue(s) found.`,
-          text: `${annotations.length} issue(s) found.`,
-          annotations,
-      }
-    })
-  } else {
-    core.info('check does not exist, creating new...')
-    const createRequest = {
-      ...context.repo,
-      accept,
-      head_sha: sha,
-      conclusion: 'success',
-      name: context.workflow,
-      status: 'completed',
-      output: {
-          title: toolName,
-          summary: `${annotations.length} issue(s) found.`,
-          text: `${annotations.length} issue(s) found.`,
-          annotations,
-      }
-    }
-    await octokit.rest.checks.create(createRequest)
-  }
 }
 
 function convertAnotationLevel(l: string | undefined): AnnotationLevel {
@@ -83,7 +48,7 @@ function convertAnotationLevel(l: string | undefined): AnnotationLevel {
     default:
       return 'notice'
   }
-  }
+}
 
 export function getAnnotationsFromSarifResult(results: Result[]): Annotation[]{
     return results.map((result) => {
